4dda691c6bc6e2748160747c6b9fa217636a1419
Jean Berniolles <jean@berniolles.fr>
2014-06-28 14:34:38 +0200

    Sort of work as a clock

--- LedMatrixClock/main.c
+++ LedMatrixClock/main.c
@@ -5,6 +5,20 @@
 
 #include "HT1632C.h"
 
+unsigned char font_mini[10][3]= {
+    {0b11111,0b10001,0b11111}, // 0
+    {0b10010,0b11111,0b10000}, // 1
+    {0b11101,0b10101,0b10111}, // 2
+    {0b10001,0b10101,0b11111}, // 3
+    {0b00111,0b00100,0b11110}, // 4
+    {0b10111,0b10101,0b11101}, // 5
+    {0b11111,0b10101,0b11101}, // 6
+    {0b00001,0b00001,0b11111}, // 7
+    {0b11111,0b10101,0b11111}, // 8
+    {0b10111,0b10101,0b11111}, // 9
+    //{0b,0b,0b},
+};
+
 unsigned char rtc_h, rtc_m, rtc_s;
 unsigned char n;
 
@@ -22,9 +36,10 @@ void framebuf_send(void) {
     unsigned char mid = 0;
     for (unsigned char i=0; i<MATRIX_COL; ++i) {
         if((0 == (i%8)) && (0 != i)) ++mid;
+        unsigned char m = _BV(i%8);
         buf[i] = 0;
         for (unsigned char j=0; j<8; ++j) {
-            if(0 != (framebuf[j + (mid*8)] & _BV(j))) {
+            if(0 != (framebuf[j + (mid*8)] & m)) {
                 buf[i] |= _BV(j);
             }
         }
@@ -32,6 +47,19 @@ void framebuf_send(void) {
     }
     
     HT1632C_Writer_AllDATA(0, buf, MATRIX_COL);
+}
+
+void framebuf_setXY(unsigned char x, unsigned char y, unsigned char pixel) {
+    if(0 == pixel)
+        framebuf[x] &= (unsigned char)0xff - _BV(y);
+    else
+        framebuf[x] |= _BV(y);
+}
+
+void framebuf_printchar(unsigned char v, unsigned char x, unsigned char y) {
+    for(unsigned char i=0; i<3; ++i) {
+        framebuf[x+i] = font_mini[v][i] << y;
+    }
 }
 
 
@@ -46,11 +74,14 @@ SIGNAL(TIMER2_COMP_vect) {
         }
     }
     
-    framebuf_clear();
+    /*framebuf_clear();
+    
     for(unsigned char i=0; i<8; i++) {
-        framebuf[i] = 0xff;
+        framebuf_printchar(i, i*4, i%4);
     }
-    framebuf_send();
+    framebuf_send();*/
+    
+    clock_printscreen_main();
     
     n++;
     n = n%MATRIX_COL;
@@ -82,6 +113,28 @@ void initIO(void) {
 }
 
 
+unsigned char util_gettensdigit(unsigned char x) {
+    return (x - (x % 10)) / 10;
+}
+
+#define MAIN_HROFF  2
+void clock_printscreen_main() {
+    framebuf_clear();
+    
+    framebuf_printchar(util_gettensdigit(rtc_h), MAIN_HROFF, 1);
+    framebuf_printchar(rtc_h % 10, MAIN_HROFF+4, 1);
+    
+    framebuf_printchar(util_gettensdigit(rtc_m), MAIN_HROFF+4+4+2, 1);
+    framebuf_printchar(rtc_m % 10, MAIN_HROFF+4+4+2+4, 1);
+    
+    framebuf_printchar(util_gettensdigit(rtc_s), MAIN_HROFF+4+4+2+4+4+1, 2);
+    framebuf_printchar(rtc_s % 10, MAIN_HROFF+4+4+2+4+4+4+1, 2);
+    
+    framebuf_setXY(MAIN_HROFF+4+3+1, 2, 1);
+    framebuf_setXY(MAIN_HROFF+4+3+1, 4, 1);
+    
+    framebuf_send();
+}
 
 int main(void) {
     _delay_ms(100);
