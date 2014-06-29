5c8efc25a28f784a11090099f4650e43c6599a9f
Jean Berniolles <jean@berniolles.fr>
2014-06-29 09:41:27 +0200

    Fixed wrong frequency for RTC. (Was 0,97Hz)

--- LedMatrixClock/main.c
+++ LedMatrixClock/main.c
@@ -83,12 +83,6 @@ SIGNAL(TIMER0_OVF_vect) {
         fsm_need_redraw = 0;
         fsm_display();
     }
-
-    /*
-    framebuf_setXY(30, 0, 0 == (PIND & _BV(MENU)) ? 1 : 0);
-    framebuf_setXY(30, 1, 0 == (PIND & _BV(UP)) ? 1 : 0);
-    framebuf_setXY(30, 2, 0 == (PIND & _BV(DOWN)) ? 1 : 0);
-    framebuf_send();*/
 }
 
 void initTick(void) {
@@ -110,7 +104,7 @@ void initRTC(void) {
     
     TCCR2 = 0;                  // Timer/counter2 disable
     TCNT2 = 0;                  // Value reset
-    OCR2 = 32;                  // We should match compare on 32
+    OCR2 = 32 - 1;                  // We should match compare on 32
     TIMSK |= _BV(OCIE2);              // Match throw signal TIMER2_COMP_vect
     ASSR |= _BV(AS2);          // Timer/Counter2 source is 32k crystal
     TCCR2 = _BV(WGM21) | _BV(CS22) | _BV(CS21) | _BV(CS20);   // CTC mode, start with prescaler value of 1024
